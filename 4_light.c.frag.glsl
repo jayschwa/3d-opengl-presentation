5478487ce5c0b94d93e9f53fe24ec0c94d7f6c49
Jay Weisskopf <jay@jayschwa.net>
2013-04-24 20:04:08 -0400

    4: Add normal attribute

---
+++ 4_light.c.frag.glsl
@@ -0,0 +1,13 @@
+#version 150
+
+uniform sampler2D tex_image;
+
+in vec3 fragment_normal;
+in vec2 fragment_tex_coords;
+
+out vec4 FragColor;
+
+void main()
+{
+	FragColor = texture(tex_image, fragment_tex_coords);
+}
--- Makefile
+++ Makefile
@@ -2,7 +2,8 @@ BINS = \
     0_empty_window.bin \
     1_triangle.bin \
     2_texture.bin \
-    3_cube.bin
+    3_cube.bin \
+    4_light.bin
 
 SHARED_OBJS = \
     shared_main.o \
